6c4d42016975e964245813838f09e8df33427cf1
Eduardo Campaña <hi@orballo.dev>
2024-08-03 22:17:33 +0200

    updates quick_test

--- crates/biome_js_formatter/tests/quick_test.rs
+++ crates/biome_js_formatter/tests/quick_test.rs
@@ -1,4 +1,4 @@
-use biome_formatter::{AttributePosition, IndentStyle, LineWidth, QuoteStyle};
+use biome_formatter::{AttributePosition, IndentStyle, IndentWidth, LineWidth, QuoteStyle};
 use biome_formatter_test::check_reformat::CheckReformat;
 use biome_js_formatter::context::{ArrowParentheses, JsFormatOptions, Semicolons};
 use biome_js_formatter::{format_node, JsFormatLanguage};
@@ -14,17 +14,8 @@ mod language {
 // use this test check if your snippet prints as you wish, without using a snapshot
 fn quick_test() {
     let src = r#"
-type InstanceID = string;
-type MaybeCardWithAttachment = string;
-function outerFunctionToForceIndent() {
-    const cardWithAttachment: (id: InstanceID) => MaybeCardWithAttachment = (
-        id
-    ) => {
-        return `${id}test`;
-    };
-}
-
     "#;
+
     let source_type = JsFileSource::tsx();
     let tree = parse(
         src,
@@ -32,18 +23,21 @@ function outerFunctionToForceIndent() {
         JsParserOptions::default().with_parse_class_parameter_decorators(),
     );
     let options = JsFormatOptions::new(source_type)
-        .with_indent_style(IndentStyle::Space)
+        .with_indent_style(IndentStyle::Tab)
+        .with_indent_width(IndentWidth::try_from(4).unwrap())
         .with_line_width(LineWidth::try_from(80).unwrap())
         .with_semicolons(Semicolons::Always)
-        .with_quote_style(QuoteStyle::Double)
+        .with_quote_style(QuoteStyle::Single)
         .with_jsx_quote_style(QuoteStyle::Single)
-        .with_arrow_parentheses(ArrowParentheses::AsNeeded)
+        .with_arrow_parentheses(ArrowParentheses::Always)
         .with_attribute_position(AttributePosition::Multiline);
 
     let doc = format_node(options.clone(), &tree.syntax()).unwrap();
     let result = doc.print().unwrap();
 
     println!("{}", doc.into_document());
+    // println!("{:#?}", doc);
+    println!();
     eprintln!("{}", result.as_code());
 
     CheckReformat::new(
